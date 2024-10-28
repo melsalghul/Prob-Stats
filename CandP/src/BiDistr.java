754e63e49e5eae0568de2887e8bfcc7a39e63da4
melsalghul <mabapo0706@gmail.com>
2024-10-28 17:05:59 -0400

    Update BiDistr.java

--- CandP/src/BiDistr.java
+++ CandP/src/BiDistr.java
@@ -2,9 +2,9 @@ import java.math.BigInteger;
 
 class BiDistr{
     public static double solve(int n, int k, double p){
-//        if (n < 0 || k < 0 || k > n || p < 0 || p > 1){
-//            return 0; 
-//        }
+        if (n < 0 || k < 0 || k > n || p < 0 || p > 1){
+            return 0; 
+        }
 
         BigInteger combinations = Comb.solve(n, k); 
         
