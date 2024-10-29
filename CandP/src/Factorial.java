534eab9704d09cda77fe1d36f66cc1dbe2332cab
melsalghul <mabapo0706@gmail.com>
2024-10-28 20:06:03 -0400

    Update Factorial.java with Documentation

--- CandP/src/Factorial.java
+++ CandP/src/Factorial.java
@@ -1,11 +1,27 @@
 import java.math.BigInteger;
 
-class Factorial{
-    public static BigInteger solve(int n){
-    	BigInteger result = BigInteger.ONE;
-    	
-        for (int i = 1; i <= n; i++)
+/**
+ * This class provides a method for calculating the factorial of a non-negative integer.
+ */
+class Factorial {
+
+    /**
+     * Calculates the factorial of a non-negative integer n (n!).
+     *
+     * @param n The non-negative integer for which to calculate the factorial.
+     * @return The factorial of n (n!) as a BigInteger.
+     * @throws IllegalArgumentException if n is negative.
+     */
+    public static BigInteger solve(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Input must be non-negative.");
+        }
+
+        BigInteger result = BigInteger.ONE;
+
+        for (int i = 1; i <= n; i++) {
             result = result.multiply(BigInteger.valueOf(i));
+        }
 
         return result;
     }
