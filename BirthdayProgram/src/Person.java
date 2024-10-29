d77965c2d663b6412bbcde098babaf9e7a44cb96
melsalghul <mabapo0706@gmail.com>
2024-10-28 20:03:22 -0400

    Update Person.java

--- BirthdayProgram/src/Person.java
+++ BirthdayProgram/src/Person.java
@@ -1,13 +1,28 @@
 import java.util.Random;
 
+/**
+ * Represents a person with a randomly assigned birthday.
+ * The birthday is represented as an integer between 1 and 365 (inclusive),
+ * where 1 represents January 1st and 365 represents December 31st.
+ */
 class Person {
-    int birthday;
 
+    private int birthday;
+
+    /**
+     * Constructs a Person object with a random birthday.
+     * The birthday is generated using a random number generator and is between 1 and 365 (inclusive).
+     */
     public Person() {
         Random temp = new Random();
         birthday = temp.nextInt(365) + 1; 
     }
 
+    /**
+     * Returns the person's birthday.
+     *
+     * @return The birthday as an integer between 1 and 365 (inclusive).
+     */
     public int getBirthday() {
         return birthday;
     }
