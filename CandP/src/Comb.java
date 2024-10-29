aeecdc3fc08e8b23d6db870a860dcfdc36c58894
melsalghul <mabapo0706@gmail.com>
2024-10-28 20:05:29 -0400

    Update Comb.java with Documentation

--- CandP/src/Comb.java
+++ CandP/src/Comb.java
@@ -1,7 +1,23 @@
 import java.math.BigInteger;
 
+/**
+ * This class provides a method for calculating combinations (nCr), also known as binomial coefficients.
+ * It uses the formula n! / (r! * (n-r)!).
+ */
 class Comb {
+
+    /**
+     * Calculates the number of combinations of n items taken r at a time (nCr).
+     *
+     * @param n The total number of items.  Must be non-negative.
+     * @param r The number of items to choose. Must be non-negative and not greater than n.
+     * @return The number of combinations (nCr) as a BigInteger.
+     * @throws IllegalArgumentException if n or r are negative, or if r is greater than n.
+     */
     public static BigInteger solve(int n, int r) {
+        if (n < 0 || r < 0 || r > n) {
+            throw new IllegalArgumentException("Invalid input: n and r must be non-negative, and r must be less than or equal to n.");
+        }
         return Factorial.solve(n).divide(Factorial.solve(r).multiply(Factorial.solve(n - r)));
     }
 }
