eaffd0f4b8bf5b1cf88806117d6fb11dc81c6a6c
melsalghul <mabapo0706@gmail.com>
2024-10-30 12:21:25 -0400

    Delete PokemonGame directory

--- PokemonGame/src/Bulbasaur.java
+++
@@ -1,8 +0,0 @@
-public class Bulbasaur extends Pokemon{
-	
-	public Bulbasaur() {
-		setName("Bulbasaur");
-		setHp(70);
-		setAttackDamage(10);
-	}
-}
--- PokemonGame/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        CardGame game = new CardGame();
-        game.run();
-    }
-}
